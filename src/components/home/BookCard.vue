00412dbe73457c5ee0063ed2bb6e1609674dc0eb
bepyan <bepyan@naver.com>
2021-02-22 17:58:53 +0900

    hover: transition

--- src/components/home/BookCard.vue
+++ src/components/home/BookCard.vue
@@ -2,11 +2,18 @@
   <v-card outlined @click="onClick">
     <v-list-item three-line>
       <v-list-item-content>
-        <v-list-item-title class="headline mb-1"> {{title}} </v-list-item-title>
-        <v-list-item-subtitle>{{publisher}}. {{auther}}.</v-list-item-subtitle>
+        <v-list-item-title class="headline mb-1">
+          {{ title }}
+        </v-list-item-title>
+        <v-list-item-subtitle
+          >{{ publisher }}. {{ auther }}.</v-list-item-subtitle
+        >
 
         <v-list-item-content>
-          <v-list-item-subtitle>총 {{stockCount}}권 중 {{stockCount-reservationCount}}권 예약 가능</v-list-item-subtitle>
+          <v-list-item-subtitle
+            >총 {{ stockCount }}권 중 {{ stockCount - reservationCount }}권 예약
+            가능</v-list-item-subtitle
+          >
         </v-list-item-content>
       </v-list-item-content>
     </v-list-item>
@@ -15,25 +22,27 @@
 
 <script>
 export default {
-  props: ['book'],
+  props: ["book"],
   data() {
     return this.book;
   },
   methods: {
     onClick() {
-      this.$router.push({path: 'detail', params: {book: this.book}})
-    }
-  }
+      this.$router.push({ path: "detail", params: { book: this.book } });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.v-card{
+.v-card {
   margin: auto;
   max-width: 700px;
   cursor: pointer;
+  transition-duration: 0.5s;
   &:hover {
-     background-color: rgba(0, 0, 0, 0.01);
+    transition-property: all;
+    background-color: rgba(0, 0, 0, 0.01);
   }
 }
 </style>
