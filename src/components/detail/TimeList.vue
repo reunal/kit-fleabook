25a74010ca99d1122476d96dfb4b350702b48a92
fhdufhdu <fhdufhdu@gmail.com>
2021-03-10 21:04:35 +0900

    [add] covid-19 warning text

--- src/components/detail/TimeList.vue
+++ src/components/detail/TimeList.vue
@@ -12,10 +12,17 @@
             <span v-for="(rsv, idx) in list" :key="(rsv, idx)" :id="idx">
               {{ rsv }}
             </span>
-            <span v-if="!list.length" class="text--secondary">none</span>
+            <span v-if="!list.length" class="text--secondary">없음</span>
           </td>
         </tr>
       </tbody>
+      <div id="coronaWarning">
+        <v-icon color="red">{{ "mdi-alert-circle" }}</v-icon>
+        <span
+          >코로나 감염 예방을 위해 많은 사람이 몰리는 것을 방지하기 위해서<br />
+          이미 다른 학우분이 예약한 시간을 피해 예약을 부탁드립니다.</span
+        >
+      </div>
     </template>
   </v-simple-table>
 </template>
@@ -39,6 +46,13 @@ export default {
 <style scoped>
 #timeList {
   text-align: center;
+}
+#coronaWarning {
+  color: red;
+  font-size: min(2.5vw, 17.5px);
+  text-align: center;
+  border: 1.5px solid rgb(212, 181, 0);
+  border-radius: min(1vw, 7px);
 }
 /* vuetify 설정 부분 건드리는 css */
 .v-data-table > .v-data-table__wrapper > table > tbody > tr > td,
@@ -77,5 +91,8 @@ export default {
   > tr
   > th {
   text-align: center;
+}
+.v-icon.v-icon {
+  font-size: min(2.5vw, 17.5px);
 }
 </style>
